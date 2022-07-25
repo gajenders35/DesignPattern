22ea9adb8d29e9ed4799537c26907e3295babc4b
Gajender <Gajenders35@gmail.com>
2022-07-25 18:31:11 +0530

    factory pattern

---
+++ src/main/java/Factory/FactoryDesign.java
@@ -0,0 +1,65 @@
+package Factory;
+
+import java.util.Scanner;
+
+interface Shape{
+	public void shape();
+	public double getArea(int x);
+}
+
+class Circle implements Shape {
+	
+	@Override
+	public void shape() {
+		System.out.println("this is circle to draw");
+	}
+	@Override
+	public double getArea(int x) {
+		return 2*7.14*x;
+	}
+}
+
+class Square implements Shape{
+	@Override
+	public void shape() {
+		System.out.println("This is Square");
+	}
+	@Override
+	public double getArea(int x) {
+		return x*x;
+	}
+	
+}
+
+class ObjectFactory{
+	
+	public static Shape getInstance(String shape) {
+		
+		if(shape.equalsIgnoreCase("circle"))
+			return new Circle();
+		
+		else if(shape.equalsIgnoreCase("Square"))
+			return new Square();
+		else
+			return null;
+	}
+	
+}
+
+public class FactoryDesign {
+	
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		
+		String str=s.next();
+		int x=s.nextInt();
+		
+		Shape shape=ObjectFactory.getInstance(str);
+		System.out.println(shape);
+		System.out.println(shape.getArea(x));
+		
+		
+	}
+
+}
